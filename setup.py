2bb1e104a8d939198a0de0d2a0e8c4ff731aaf46
kaemiin <kaemiin@xinosys.com>
2018-09-20 12:06:09 +0800

    edit README.rst

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='pythontemplate',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Python Template',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='kaemiin',
+    author_email='kaemiinwork@gmail.com',
+    url='https://github.com/kaemiin',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     cmdclass={
